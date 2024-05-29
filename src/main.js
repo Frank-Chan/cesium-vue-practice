dde1d37b2c9d65276c2e9c6366fba4daf86a70e0
Frank-Chan <gisfeifly@gmail.com>
2024-05-29 18:32:16 +0800

    Doc:补充代码注释

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//创建应用程序实例，并在指定dom元素（'app'）上装载应用程序实例的根组件
 createApp(App).mount('#app')
 // The URL on your server where CesiumJS's static files are hosted.
 window.CESIUM_BASE_URL = "/";
