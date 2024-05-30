dcef005a2b5a6cde96a28712f64ee419e9685d86
Frank-Chan <gisfeifly@gmail.com>
2024-05-30 17:06:14 +0800

    Feat:引入ElementPlus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "element-plus/dist/index.css"; //引入ElementPlus的样式
 
 //创建应用程序实例，并在指定dom元素（'app'）上装载应用程序实例的根组件
-createApp(App).mount('#app')
+createApp(App).mount("#app");
 // The URL on your server where CesiumJS's static files are hosted.
 window.CESIUM_BASE_URL = "/";
